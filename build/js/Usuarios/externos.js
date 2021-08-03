49d205732fa67df1447b3c76fd80c0a577f7dd95
jhruiz <jaiber.ruiz@correounivalle.edu.co>
2021-08-03 00:33:36 -0500

    primer commit de la aplicacion de configuracion administrativa

---
+++ build/js/Usuarios/externos.js
@@ -0,0 +1,21 @@
+/**
+ * Setea el div content con el formulario para crear un nuevo usuario
+ */
+ function nuevoUsuario() {
+    $.ajax({
+        method: "GET",
+        url: '../../pages/usuarios/add.html',
+        success: function(respuesta) {
+            $('#content-data').html(respuesta);         
+        },
+        error: function() {
+            var mensaje = 'Se presentó un error. Por favor, inténtelo mas tarde.';
+            sweetMessage('error', mensaje);
+        }
+      })     
+}
+
+$( document ).ready(function() {
+    nuevoUsuario();
+    $('#content-usuarios').css("margin-top", "300px !important");
+});
